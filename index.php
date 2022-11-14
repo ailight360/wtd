8461af4c7c1fadfc49ffa1c977b9d7660ae97f45
ailight360 <118069640+ailight360@users.noreply.github.com>
2022-11-14 19:04:01 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> 
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
+	<title><?php bloginfo('name'); ?></title>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="wrap">
+
+        <div id="header">
+            <h1>Blog Template</a></h1>
+            <h2>Website description</h2>
+        </div>
+
+        <div id="menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Sites</a></li>
+                <li><a href="#">Resume</a></li>
+                <li><a href="#">References</a></li>
+                <li><a href="#">Contact Me</a></li>
+            </ul>
+        </div>
+
+        <div id="content">
+
+            <div class="left">
+
+							<div class="blog_post">
+								<h2>This is a blog post</h2>
+
+								<p>I created this from an existing template I recieved while researching. The point of this project was
+								to see if I can recreavailable at Free CSS Templates through the
+								<strong>Creative Commons Attributes 2.5</strong> license. </p>
+								<p>I really hope you all read the two paragraphs above, as they contain copywrite and creative commons
+								licensing information. However, I havback to this code another time. I'm learning
+								PHP and how to create Wordpress templates. This design will be the one I'll use for testing.</p>
+
+								<a class="readmore" href="#">Read More...</a>
+
+							</div>
+
+							<div class="blog_post">
+								<h2>This is a blog post</h2>
+								<p>I created this from an existing template I recieved while researching. The point of this project was
+								to see if I can recreavailable at Free CSS Templates through the
+								<strong>Creative Commons Attributes 2.5</strong> license. </p>
+								<p>I really hope you all read the two paragraphs above, as they contain copywrite and creative commons
+								licensing information. However, I havback to this code another time. I'm learning
+								PHP and how to create Wordpress templates. This design will be the one I'll use for testing.</p>
+
+							</div>
+
+           </div>
+
+
+            <div class="right">
+                <h2>Categories</h2>
+                <ul>
+                    <li><a href="#">HTML 5 Tricks</a></li>
+                    <li><a href="#">CSS3 Styles</a></li>
+                    <li><a href="#">Wordpress</a></li>
+                    <li><a href="#">Open Source</a></li>
+                    <li><a href="#">Python Apps</a></li>
+                    <li><a href="#">Data Science</a></li>
+                </ul>
+                <h2>Archives</h2>
+                <ul>
+                    <li><a href="#">January 2015</a></li>
+                    <li><a href="#">February 2015</a></li>
+                    <li><a href="#">March 2015</a></li>
+                    <li><a href="#">April 2015</a></li>
+                    <li><a href="#">May 2015</a></li>
+                    <li><a href="#">June 2015</a></li>
+                    <li><a href="#">July 2015</a></li>
+                    <li><a href="#">August 2015</a></li>
+                    <li><a href="#">September 2015</a></li>
+                    <li><a href="#">October 2015</a></li>
+                    <li><a href="#">November 2015</a></li>
+                    <li><a href="#">December 2015</a></li>
+                </ul>
+            </div>
+            <div style="clear:both;"></div>
+            <div id="bottom"></div>
+            <div id="footer">
+                designed with guidance
+            </div>
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
